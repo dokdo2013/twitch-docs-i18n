755a789d0e9b0e6f3376470786b037d628c10445
hyeonwoojo <h_jo@ejn.gg>
2023-04-29 19:00:47 +0900

    init: Init main project

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
 });
 
 module.exports = withNextra({
+  reactStrictMode: true,
   i18n: {
     locales: ["en", "ko"],
     defaultLocale: "en",
   },
+  // trailingSlash: true,
 });
